82e4414067c4e120a0e3599c1c3de16476330f12
dansigit <e.dansi.111@gmail.com>
2019-08-06 10:27:24 +0430

    message

--- app/src/main/java/com/example/mypc/testgit/MainActivity.java
+++ app/src/main/java/com/example/mypc/testgit/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mypc.testgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"branch1",Toast.LENGTH_LONG).show();
     }
 }

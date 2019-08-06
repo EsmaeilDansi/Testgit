70ca80567aeeeecc12c1d1f0ba63fb346344c2f0
dansigit <e.dansi.111@gmail.com>
2019-08-06 10:30:49 +0430

    message

--- app/src/main/java/com/example/mypc/testgit/MainActivity.java
+++ app/src/main/java/com/example/mypc/testgit/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(),"branch1",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"Branch2",Toast.LENGTH_LONG).show();
     }
 }
